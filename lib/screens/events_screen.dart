7c7ce722c29bb5c032eadcd369d9ed0b34471246
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-10-10 02:58:42 +0800

    Update events_screen.dart

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:team_23/providers/event.dart';
 import 'package:team_23/providers/events.dart';
-import 'package:team_23/screens/EventTile.dart';
+import 'package:team_23/widgets/event_tile.dart';
 
 class EventsScreen extends StatefulWidget {
   @override
@@ -24,11 +24,10 @@ class _EventsScreenState extends State<EventsScreen> {
     return Scaffold(
         body: Container(
             child: ListView.builder(
-                itemCount: events.length,
-                itemBuilder: (context, index) {
-                  return EventTile(event: events[index]);
-                },
-              ))
-        );
+      itemCount: events.length,
+      itemBuilder: (context, index) {
+        return EventTile(events[index]);
+      },
+    )));
   }
 }
