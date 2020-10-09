41d5f5f7bf5d379735db050cd44e78e5faf10305
Walkier <chowwalker@gmail.com>
2020-10-09 23:31:24 +0800

    added ./build/ to git ignore feat: Non-dynamic ListView of Events

--- lib/providers/event.dart
+++ lib/providers/event.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 class Event with ChangeNotifier {
   final String id;
-  final DateTime date;
-  final String imageUrl;
-  final int age;
-  final String eventType;
+  final String title;
+  final int ageMin;
+  final int ageMax;
+  final DateTime datePost; //TODO: convert back to datetime
+  final DateTime dateDeadline;
+  final DateTime dateEvent;
+  final String tag;
+  final String thumbUrl;
+  final int users;
+  final int usersMax;
 
-  Event(this.id, this.date, this.imageUrl, this.age, this.eventType);
+  Event(this.id, this.title, this.ageMin, this.ageMax, this.datePost,
+      this.dateDeadline, this.dateEvent, this.tag, this.thumbUrl,
+      this.users, this.usersMax);
 }
